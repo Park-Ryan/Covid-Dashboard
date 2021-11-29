7cde1753231e731b0d82d8cbebd159c07f7bb1f8
Moltenfuzzy <kentphan2000@gmail.com>
2021-11-29 14:19:24 -0800

    final fixes

--- covid_dashboard/frontend/src/components/App.js
+++ covid_dashboard/frontend/src/components/App.js
@@ -1,28 +1,47 @@
 import React, { Component } from "react";
 import { render } from "react-dom";
-import { BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import Analytics from "./pages/Analytics";
+import TextAnalytics from "./pages/TextAnalytics";
 import Dashboard from "./dashboard/Dashboard";
 
 // callbacks, pass child data back to parent component
 export default class App extends Component {
 	constructor(props) {
 		super(props);
+
+		this.state = {
+			mainData: null,
+		};
+		this.setMainData = this.setMainData.bind(this);
+	}
+
+	setMainData(data) {
+		this.setState({
+			mainData: data,
+		});
+		console.log("Hello from setMainData in App.js, data stored : ");
+		console.log(data);
 	}
 
 	render() {
 		return (
 			// <div className="initLayout">
-            <div>
+			<div>
 				<Router>
 					<Switch>
-						<Route
+						{/* <Route
 							path="/"
 							exact
 							component={Dashboard}
-						/>
-						<Route path="/Analytics" exact component={Analytics} />
+							grandparentCallback={this.setMainData}
+						/> */}
+						<Route exact path="/">
+							<Dashboard grandparentCallback={this.setMainData} />
+						</Route>
+						<Route path="/Analytics"> <Analytics/> </Route>
+						<Route path="/TextAnalytics"> <TextAnalytics/> </Route>
 						<Route path="/HomePage" exact component={HomePage} />
 					</Switch>
 				</Router>
--- covid_dashboard/frontend/src/components/BarChart.js
+++ covid_dashboard/frontend/src/components/BarChart.js
@@ -1,52 +1,100 @@
-import * as React from 'react';
-import Paper from '@mui/material/Paper';
+// import * as React from 'react';
+// import Paper from '@mui/material/Paper';
+// import {
+//   Chart,
+//   BarSeries,
+//   Title,
+//   ArgumentAxis,
+//   ValueAxis,
+// } from '@devexpress/dx-react-chart-material-ui';
+// import { Animation } from '@devexpress/dx-react-chart';
+
+// const data = [
+//   { year: '1950', population: 2.525 },
+//   { year: '1960', population: 3.018 },
+//   { year: '1970', population: 3.682 },
+//   { year: '1980', population: 4.440 },
+//   { year: '1990', population: 5.310 },
+//   { year: '2000', population: 6.127 },
+//   { year: '2010', population: 6.930 },
+// ];
+
+// export default class Demo extends React.PureComponent {
+//   constructor(props) {
+//     super(props);
+
+//     this.state = {
+//       data,
+//     };
+//   }
+
+//   render() {
+//     const { data: chartData } = this.state;
+
+//     return (
+//       <Paper>
+//         <Chart
+//           data={chartData}
+//         >
+//           <ArgumentAxis />
+//           <ValueAxis max={7} />
+
+//           <BarSeries
+//             valueField="population"
+//             argumentField="year"
+//           />
+//           <Title text="World population" />
+//           <Animation />
+//         </Chart>
+//       </Paper>
+//     );
+//   }
+// }
+
+import * as React from "react";
+import Paper from "@mui/material/Paper";
 import {
-  Chart,
-  BarSeries,
-  Title,
-  ArgumentAxis,
-  ValueAxis,
-} from '@devexpress/dx-react-chart-material-ui';
-import { Animation } from '@devexpress/dx-react-chart';
+	ArgumentAxis,
+	ValueAxis,
+	Chart,
+	BarSeries,
+} from "@devexpress/dx-react-chart-material-ui";
+import { modalClasses } from "@mui/material";
+import { CountryData } from "./data/CountryData";
 
-const data = [
-  { year: '1950', population: 2.525 },
-  { year: '1960', population: 3.018 },
-  { year: '1970', population: 3.682 },
-  { year: '1980', population: 4.440 },
-  { year: '1990', population: 5.310 },
-  { year: '2000', population: 6.127 },
-  { year: '2010', population: 6.930 },
-];
+function createData(country, deaths) {
+	return {
+		country: country,
+		deaths: deaths,
+	};
+}
 
-export default class Demo extends React.PureComponent {
-  constructor(props) {
-    super(props);
+export default function BarChart({data}) {
+	const [newData, setNewData] = React.useState([]);
 
-    this.state = {
-      data,
-    };
-  }
+	const UpdateChart = (data) => {
+		let temp = [];
+		for (let i = 0; i < data.length; i++) {
+			temp.push(
+				createData(data[i]["Country"], data[i]["Types"]["Deaths"])
+			);
+		}
+		console.log(temp);
+		setNewData(temp);
+	};
 
-  render() {
-    const { data: chartData } = this.state;
+	React.useEffect(() => {
+		UpdateChart(data);
+	}, [data]);
 
-    return (
-      <Paper>
-        <Chart
-          data={chartData}
-        >
-          <ArgumentAxis />
-          <ValueAxis max={7} />
-
-          <BarSeries
-            valueField="population"
-            argumentField="year"
-          />
-          <Title text="World population" />
-          <Animation />
-        </Chart>
-      </Paper>
-    );
-  }
+	console.log(data);
+	return (
+		<Paper>
+			<Chart data={newData}>
+				<ArgumentAxis />
+				<ValueAxis />
+				<BarSeries valueField="deaths" argumentField="country" />
+			</Chart>
+		</Paper>
+	);
 }
